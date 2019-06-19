6248f3d613a612b1828dd1aa7adab68b2c98380e
ZlatkoMiceski <45883335+ZlatkoMiceski@users.noreply.github.com>
2019-06-19 18:10:56 +0200

    Add files via upload

---
+++ CustomOne/index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Maybe a CustomOne site</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+
+
+</head>
+
+<body>
+    <div class="container-fluid socialmedia">
+        <div class="row socialmediarow">
+            <i class="fa fa-facebook" style="font-size:20px"></i>
+            <i class="fa fa-twitter" style="font-size:20px"></i>
+            <i class="fa fa-skype" style="font-size:20px"></i>
+            <i class="fa fa-linkedin" style="font-size:20px"></i>
+            <i class="fa fa-google-plus" style="font-size:20px"></i>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row mmm">
+            <div class="col-sm-5 mmm1">
+                <img src="enfold.png">
+            </div>
+            <div class="col-sm-7">
+                <div class="navbar navbar-default" role="navigation">
+                    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <div class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav navbar-right marks">
+                            <li class="active"><a href="#" style="color: black;background-color: white"><strong>Home</strong></a></li>
+                            <li><a href="#"><strong>Features</strong></a></li>
+                            <li><a href="#"><strong>Headers</strong></a></li>
+                            <li><a href="#"><strong>Portfolio</strong></a></li>
+                            <li><a href="#"><strong>Blog</strong></a></li>
+                            <li><a href="#"><strong>Pages</strong></a></li>
+                            <li><a href="#"><strong>Shortcodes</strong></a></li>
+                            <li><a href="#"><strong>Shop</strong></a></li>
+                            <li><a href="#"><strong><i class="material-icons">search</i></strong></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+
+                <div class="item active">
+                    <img src="fitt3.jpg" alt="Los Angeles" style="width:100%;">
+                    <div class="carousel-caption">
+                        <h3>Los Angeles</h3>
+                        <p>LA is always so much fun!</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="fitt4.jpg" alt="Chicago" style="width:100%;">
+                    <div class="carousel-caption">
+                        <h3>Chicago</h3>
+                        <p>Thank you, Chicago!</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="fitt5.jpg" alt="New York" style="width:100%;">
+                    <div class="carousel-caption">
+                        <h3>New York</h3>
+                        <p>We love the Big Apple!</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-lg-4">
+                <span class="circle"></span>
+                <h3>CHECK IT!</h3>
+                <p>Some text in order to <a href="#">appear</a> like something like nothing else, maybe better</p>
+            </div>
+            <div class="col-lg-4">
+                <span class="circle"></span>
+                <h3>LIGHTNING FAST!</h3>
+                <p>Some text in order to appear like <a href="#"> something like</a> nothing else, maybe better</p>
+            </div>
+            <div class="col-lg-4">
+                <span class="circle"></span>
+                <h3>GREEN POWER</h3>
+                <p>Some text in order to appear like something like <a href="#">nothing else</a> maybe better</p>
+            </div>
+        </div>
+
+        <div class="row text-left">
+            <div class="col-lg-3">
+                <h4><span class="glyphicon glyphicon-phone"></span>  MOBILE READY</h4>
+                <p>Some text in order to appear like something like <a href="#">nothing else</a> maybe better.
+                    A little more to it in order to look even better. Thanks for your enter what.</p>
+            </div>
+            <div class="col-lg-3">
+                <h4><span class="glyphicon glyphicon-search"></span>  SEO OPTIMIZED</h4>
+                <p>Some text in order to appear like something like <a href="#">nothing else</a> maybe better.
+                    A little more to it in order to look even better. Thanks for your enter what.</p>
+            </div>
+            <div class="col-lg-3">
+                <h4><span class="glyphicon glyphicon-link"></span>  FREE SUPPORT</h4>
+                <p>Some text in order to appear like something like <a href="#">nothing else</a> maybe better.
+                    A little more to it in order to look even better. Thanks for your enter what.</p>
+            </div>
+            <div class="col-lg-3">
+                <h4><span class="glyphicon glyphicon-refresh"></span>   UPDATES</h4>
+                <p>Some text in order to appear like something like <a href="#">nothing else</a> maybe better.
+                    A little more to it in order to look even better. Thanks for your enter what.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid footercolor">
+        <div class="container">
+            <div class="row footer">
+                <div class="col-lg-1"></div>
+                <div class="col-lg-2">
+                    <h3>PRODUCT</h3>
+                    <p>Business</p>
+                    <p>Compare</p>
+                    <p>Features</p>
+                    <p>Pricing</p>
+                    <p>Mobile</p>
+                    <p>App Showcase</p>
+                </div>
+                <div class="col-lg-2">
+                    <h3>COMPANY</h3>
+                    <p>About Us</p>
+                    <p>Blog</p>
+                    <p>News</p>
+                    <p>Press Realises</p>
+                    <p>Jobs</p>
+                    <p>Contact Us</p>
+                </div>
+                <div class="col-lg-2">
+                    <h3>LEARN MORE</h3>
+                    <p>Support</p>
+                    <p>Developers</p>
+                    <p>Referral Program</p>
+                    <p>Affiliate Program</p>
+                    <p>Resseler Program</p>
+                    <p>FAQ</p>
+                </div>
+                <div class="col-lg-4">
+                    <h3>CONNECT WITH US</h3>
+                    <a href="#" class="fa fa-facebook"><span>Facebook</span></a>
+                    <a href="#" class="fa fa-twitter second"><span>Twitter</span></a>
+                    <br>
+                    <a href="#" class="fa fa-google"><span>Google</span></a>
+                    <a href="#" class="fa fa-youtube second"><span>Youtube</span></a>
+                    <br>
+                    <a href="#" class="fa fa-linkedin"><span>Linkedin</span></a>
+                    <a href="#" class="fa fa-rss second"><span>Blog</span></a>
+                </div>
+            </div>
+            <div class="row footer2">
+                <div class="col-lg-4">
+                    <p>© 2019, All Rights Reserved</p>
+                </div>
+                <div class="col-lg-8 text-right">
+                    <a href="#">Terms of Service |</a>
+                    <a href="#">Privacy Policy |</a>
+                    <a href="#">Site map</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <h1>Start</h1>
+
+    <p id="demo"></p>
+
+    <script src="javascript.js"></script>
+</body>
+</html>
